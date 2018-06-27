a76b5f83d8acf6a2534fbfcb7156b001b6ab4294
Jackson Zhang <zbcjackson@gmail.com>
2018-06-27 16:24:00 +0800

    Refactor Dashboard to pure function

--- app/containers/DashboardPage.js
+++ app/containers/DashboardPage.js
@@ -1,13 +1,9 @@
 import React from 'react';
 
-export default class DashboardPage extends React.Component {
-  render() {
-    return (
-      <div style={{minHeight: 700}}>
-        <h1>
-          Dashboard
-        </h1>
-      </div>
-    )
-  }
-}
+export default ()=>(
+  <div style={{minHeight: 700}}>
+    <h1>
+      Dashboard
+    </h1>
+  </div>
+)
