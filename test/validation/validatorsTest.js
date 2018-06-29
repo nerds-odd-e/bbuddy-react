3f78bf98aeea0ce85a644c7f4383a6fbb017766b
Jackson Zhang <zbcjackson@gmail.com>
2018-06-29 19:11:13 +0800

    Add more tests for validators

--- test/validation/validatorsTest.js
+++ test/validation/validatorsTest.js
@@ -2,24 +2,36 @@ import {required, email, number} from '../../app/validation/validators'
 
 describe('Validators', () => {
   describe('required', () => {
-    it('test whether value is empty', () => {
+    it('invalid when value is empty', () => {
       required.invalid('').should.be.true
+    })
+    it('valid when value is not empty', () => {
+      required.invalid('aaa').should.be.false
     })
     it('error message format', () => {
       required.message('name').should.be.eql('Name should not be empty')
     })
   })
   describe('email', () => {
-    it('test whether email is valid', () => {
+    it('invalid when email format is wrong', () => {
       email.invalid('aaa').should.be.true
+    })
+    it('valid when email format is good', () => {
+      email.invalid('valid@email.com').should.be.false
     })
     it('error message format', () => {
       email.message('email').should.be.eql('Email is not valid')
     })
   })
   describe('number', () => {
-    it('test whether number is valid', () => {
+    it('invalid when value is letters', () => {
       number.invalid('aaa').should.be.true
+    })
+    it('valid when number is valid', () => {
+      number.invalid('10').should.be.false
+    })
+    it('negative is valid', () => {
+      number.invalid('-10').should.be.false
     })
     it('error message format', () => {
       number.message('balance').should.be.eql('Balance is not a valid number')
